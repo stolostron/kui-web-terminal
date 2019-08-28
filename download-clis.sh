c2f041e3ccd2e43bdd962c8e5ae482b20625865a
Hanqiu Zhang <Hanqiu.Zhang@ibm.com>
2019-08-28 15:49:53 -0400

    run power build in pr (#101)
    * run power build in pr
    
    * update travis.yml to fix pr build
    
    * add more ls
    
    * use docker cp instead of docker run to download clis
    
    * another try
    
    * add download checks
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * again
    
    * try
    
    * try
    
    * try with new method
    
    * clean up the script
    
    * make sure we can push to scratch
    
    * restore travis

--- download-clis.sh
+++ download-clis.sh
@@ -16,30 +16,52 @@ DOCKER_REGISTRY=hyc-cloud-private-integration-docker-local.artifactory.swg-devop
 DOCKER_NAMESPACE=ibmcom
 CLOUDCTL_VER=latest
 CLOUDCTL_IMAGE=$DOCKER_REGISTRY/$DOCKER_NAMESPACE/icp-platform-api-$ARCH:$CLOUDCTL_VER
-ICP_VER=4.1.0
+ICP_VER=3.2.1
 
 rm -rf downloads
 mkdir -p downloads
 
 echo "Downloading cloudctl $CLOUDCTL_IMAGE ..."
 docker pull $CLOUDCTL_IMAGE
-docker run -u root --entrypoint sh -v $(pwd)/downloads:/downloads $CLOUDCTL_IMAGE -c "cp /etc/platform-api/public/cli/cloudctl-linux-${ARCH} /downloads/"
-echo "Downloaded cloudctl-linux-${ARCH} to downloads/"
+
+echo "running cloudctl image for file downloads" 
+CLOUDCTL_CONTAINER_NAME=temp-cont-download-cli
+docker run --name ${CLOUDCTL_CONTAINER_NAME}  -d --entrypoint tail $CLOUDCTL_IMAGE -f /dev/null
+CLOUDCTL_CONTAINER_ID=`docker ps -f name=temp-cont-download-cli -q`
+echo "container id: ${CLOUDCTL_CONTAINER_ID}" 
+
+echo "Downloading cloudctl ..."
+docker cp ${CLOUDCTL_CONTAINER_ID}:"/etc/platform-api/public/cli/cloudctl-linux-${ARCH}" $(pwd)/downloads/
 
 echo "Downloading istioctl ..."
-docker run -u root --entrypoint sh -v $(pwd)/downloads:/downloads $CLOUDCTL_IMAGE -c "cp /etc/platform-api/public/cli/istioctl-linux-${ARCH} /downloads/"
-echo "Downloaded istioctl-linux-${ARCH} to downloads/"
+docker cp ${CLOUDCTL_CONTAINER_ID}:"/etc/platform-api/public/cli/istioctl-linux-${ARCH}" $(pwd)/downloads/
 
 echo "Downloading kubectl ..."
-docker run -u root --entrypoint sh -v $(pwd)/downloads:/downloads $CLOUDCTL_IMAGE -c "cp /etc/platform-api/public/cli/kubectl-linux-${ARCH} /downloads/"
-echo "Downloaded kubectl-linux-${ARCH} to downloads/"
+docker cp ${CLOUDCTL_CONTAINER_ID}:"/etc/platform-api/public/cli/kubectl-linux-${ARCH}" $(pwd)/downloads/
 
 echo "Downloading helm ..."
-docker run -u root --entrypoint sh -v $(pwd)/downloads:/downloads $CLOUDCTL_IMAGE -c "cp /etc/platform-api/public/cli/helm-linux-${ARCH}.tar.gz /downloads/"
+docker cp ${CLOUDCTL_CONTAINER_ID}:"/etc/platform-api/public/cli/helm-linux-${ARCH}.tar.gz" $(pwd)/downloads/
+
+# stop container
+echo "stop container"
+docker stop $CLOUDCTL_CONTAINER_NAME
+docker rm  $CLOUDCTL_CONTAINER_NAME
+echo "ls -l downloads"
+ls -l downloads
+
+echo "checking results"
+[[ ! -f "downloads/cloudctl-linux-${ARCH}" ]] && echo "download cloudctl failed" && exit -1
+echo "Downloaded cloudctl-linux-${ARCH} to downloads/"
+[[ ! -f "downloads/istioctl-linux-${ARCH}" ]] && echo "download istioctl failed" && exit -1
+echo "Downloaded istioctl-linux-${ARCH} to downloads/"
+[[ ! -f "downloads/kubectl-linux-${ARCH}" ]] && echo "download istioctl failed" && exit -1
+echo "Downloaded istioctl-linux-${ARCH} to downloads/"
+[[ ! -f "downloads/helm-linux-${ARCH}.tar.gz" ]] && echo "download helm failed" && exit -1
 echo "Downloaded helm-linux-${ARCH}.tar.gz to downloads/"
 
 if [ "$ARCH" = "amd64" ]; then
   echo "Downloading oc ..."
   curl -fksSL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.11/openshift-client-linux-4.1.11.tar.gz | tar -xvz -C ./downloads/ oc
+  [[ ! -f "downloads/oc" ]] && echo "download oc failed" && exit -1
   echo "Downloaded openshift origin to downloads/"
 fi
