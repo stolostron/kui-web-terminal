f8d220bef0182cd4b2f41e652c167f78c78f3840
Connor E Wyckoff <Connor.Wyckoff@ibm.com>
2019-09-06 15:06:15 -0400

    correct test.js file (#138)

---
+++ client/tests/e2e/main.test.js
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2019. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
+
+
+module.exports = {
+  before: function (browser) {
+    const KUI =  browser.page.KUI()
+    const loginPage = browser.page.LoginPage()
+    if (process.env.TEST_URL && (!process.env.TEST_URL.includes('localhost'))) {
+      loginPage.navigate()
+      loginPage.authenticate()
+    }
+    KUI.navigate()
+    KUI.waitForPageLoad(browser)
+    KUI.verifyWebsocketConnection(browser)
+  },
+
+  'Verify KUI getting started command': browser => {
+    const KUI = browser.page.KUI()
+    KUI.executeCommand(browser, 'getting started')
+    KUI.verifySidecar()
+  },
+
+  'Verify cloudctl login success': browser => {
+    const KUI = browser.page.KUI()
+    const CLI = browser.page.CLI()
+    KUI.executeCommand(browser, 'cloudctl tokens')
+    CLI.verifyUserAuthenticated(browser)
+  },
+
+  'Verify supported themes': browser => {
+    const KUI = browser.page.KUI()
+    const themes = [{ name: 'IBM Dark' }, { name: 'IBM Light' }] // come back to this, evaluated color values keep changing
+    KUI.executeCommand(browser, 'themes')
+    themes.forEach(theme => KUI.verifyTheme(browser, theme))
+  },
+
+  'Verify product header': browser => {
+    const KUI = browser.page.KUI()
+    KUI.verifyProductHeader()
+  },
+
+  'Verify KUI multiple tabs': browser => {
+    const KUI = browser.page.KUI()
+    KUI.verifyNewTabs(browser)
+  },
+
+  after: function (browser, done) {
+    setTimeout(() => {
+      if (browser.sessionId) {
+        browser.end()
+        done()
+      } else {
+        done()
+      }
+    })
+  }
+}
