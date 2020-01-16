c98395a558d575d9a1ba352c7dc80f5f223d610d
Hanqiu Zhang <hanqiu.zhang@ibm.com>
2020-01-16 16:13:18 -0500

    add scripts for apply extensions

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+if [ ! -z ${ENABLE_EXTENSIONS} ] && [ ${ENABLE_EXTENSIONS} = 'true' ] ; then
+    echo 'Extensions enabled'
+    echo 'Moving extensions'
+    if [ ! -z "${DOWNLOAD_PATH}" ] && [ -d "${DOWNLOAD_PATH}" ] && [ -d "${DOWNLOAD_PATH}/${CLI_FOLDER}" ] ; then
+        for filename in "${DOWNLOAD_PATH}/${CLI_FOLDER}"/* ; do
+            if [ -f "${filename}" ] ; then
+                echo "chmod a+x ${filename} && mv ${filename} /usr/local/bin/ "
+                chmod a+x "${filename}" && mv "${filename}" /usr/local/bin/ 
+            fi
+        done
+    fi
+fi
+echo 'starting'
+#npm start
