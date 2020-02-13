44547d1948a5d4c050572fc5a463476556a48693
Sho Weimer <US2J2787@ibm.com>
2020-02-13 11:39:36 -0500

    commit tests and re-add publish stage

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -3,18 +3,4 @@ set -e
 
 export DOCKER_IMAGE_AND_TAG=${1}
 
-function fold_start() {
-  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
-}
-
-function fold_end() {
-  echo -e "\ntravis_fold:end:$1\r"
-}
-
-fold_start tests "TESTS"
 make run-all-tests
-fold_end tests
-
-fold_start logs "LOGS"
-docker logs kui-web-terminal
-fold_end logs
