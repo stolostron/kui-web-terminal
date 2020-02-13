1932a6b05d05ee58c84bb4f0449cc02321a45429
Sho Weimer <US2J2787@ibm.com>
2020-02-13 10:45:01 -0500

    fix bug with travis folding

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
-source ./build.sh
-
 export DOCKER_IMAGE_AND_TAG=${1}
+
+function fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+function fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
 
 fold_start tests "TESTS"
 make run-all-tests
