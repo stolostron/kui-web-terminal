755ca86c6e1232890c1110c0bfc283d87d1e1342
Hanqiu Zhang <hanzhang@redhat.com>
2020-05-06 09:47:06 -0400

    use hack for react

--- client/client-default/index.js
+++ client/client-default/index.js
@@ -8,10 +8,5 @@
  *******************************************************************************/
 'use strict'
 
-/**
- * Here we arrange the CSS for base functionality of Kui. Order is
- * preserved in the resulting <link> tags.
- *
- */
-
-require('@kui-shell/plugin-client-common') // plugin-client-common includes the basic styling of Kui
+// use alternate directly
+module.exports = require('@kui-shell/plugin-client-alternate') // plugin-client-common includes the basic styling of Kui
