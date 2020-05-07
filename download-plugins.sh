ae686830dc627cb2a7140bf098cdeae626deb400
dislbenn <lavontae.bennett@gmail.com>
2020-05-07 09:58:05 -0400

    bump search version up to v2.0

--- download-plugins.sh
+++ download-plugins.sh
@@ -52,7 +52,7 @@ rm -rf plugin-downloads
 mkdir plugin-downloads
 
 echo "Downloading plugins ..."
-download "search-kui-plugin" "plugin-search" "v1.0.8"
+download "search-kui-plugin" "plugin-search" "v2.0.0"
 download "plugin-kui-addons" "plugin-kui-addons" "v1.0.0"
 
 echo "plugin-downloads:"
