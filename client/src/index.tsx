9ef26b73f5f65b3d8310113175eab15738c33317
Hanqiu Zhang <hanzhang@redhat.com>
2020-06-03 11:47:31 -0400

    add local search

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -28,13 +28,14 @@ import {
   CurrentContext,
   CurrentNamespace,
 } from "@kui-shell/plugin-kubectl/components";
+import CustomSearchInput from "@kui-shell/plugin-search/mdist/components/CustomSearchInput"
 import { ClusterUtilization } from "@kui-shell/plugin-kubectl/view-utilization";
 import { ProxyOfflineIndicator } from "@kui-shell/plugin-proxy-support";
 import { productName } from '@kui-shell/client/config.d/name.json'
 const wrapper = document.querySelector(".main");
 if (wrapper) {
   render(
-    <Kui bottomInput productName={productName}>
+    <Kui bottomInput={<CustomSearchInput/>} productName={productName}>
       <ContextWidgets>
         <CurrentContext />
         <CurrentNamespace />
