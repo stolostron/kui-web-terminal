c73a508420ab6311415920518bf861bb4ff759d1
Robert Howington <rhowingt@redhat.com>
2020-06-18 16:29:29 -0400

    rhowingt/search plugin (#106)
    * add search back to client-update-plugin recipe
    
    * hardcode input
    
    * add local search
    
    * don't remove when downloads
    
    * update prompt
    
    * update search
    
    * update search plugin
    
    * remove redund identifier
    
    * build: update search-plugin to 2.1.0 - update deps
    
    Co-authored-by: Hanqiu Zhang <hanzhang@redhat.com>
    Co-authored-by: Mark Anderson <51764173+mark-nc@users.noreply.github.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -28,13 +28,14 @@ import {
   CurrentContext,
   CurrentNamespace,
 } from "@kui-shell/plugin-kubectl/components";
+import CustomSearchInput from "@kui-shell/plugin-search/mdist/components/CustomSearchInput"
 import { ClusterUtilization } from "@kui-shell/plugin-kubectl/view-utilization";
 import { ProxyOfflineIndicator } from "@kui-shell/plugin-proxy-support";
 import { productName, connectSuccess } from '@kui-shell/client/config.d/name.json'
 const wrapper = document.querySelector(".main");
 if (wrapper) {
   render(
-    <Kui bottomInput productName={productName} loadingDone={() => <pre>{connectSuccess}</pre>} noPromptContext prompt="&#x276f;">
+    <Kui bottomInput={<CustomSearchInput/>} productName={productName} loadingDone={() => <pre>{connectSuccess}</pre>} noPromptContext prompt="&#x276f;">
       <ContextWidgets>
         <CurrentContext />
         <CurrentNamespace />
