d883bd736a56d6d3a0614afc3cc3a51086be86e9
Subbarao Meduri <smeduri@redhat.com>
2020-06-23 15:24:18 -0400

    vendorize 2.0 release (#123)

---
+++ vendor/client/node_modules/monaco-editor-webpack-plugin/out/languages.d.ts
@@ -0,0 +1,3 @@
+import { IFeatureDefinition } from "./types";
+export declare const languagesArr: IFeatureDefinition[];
+export declare type EditorLanguage = 'abap' | 'apex' | 'azcli' | 'bat' | 'cameligo' | 'clojure' | 'coffee' | 'cpp' | 'csharp' | 'csp' | 'css' | 'dockerfile' | 'fsharp' | 'go' | 'graphql' | 'handlebars' | 'html' | 'ini' | 'java' | 'javascript' | 'json' | 'kotlin' | 'less' | 'lua' | 'markdown' | 'mips' | 'msdax' | 'mysql' | 'objective-c' | 'pascal' | 'pascaligo' | 'perl' | 'pgsql' | 'php' | 'postiats' | 'powerquery' | 'powershell' | 'pug' | 'python' | 'r' | 'razor' | 'redis' | 'redshift' | 'restructuredtext' | 'ruby' | 'rust' | 'sb' | 'scheme' | 'scss' | 'shell' | 'solidity' | 'sophia' | 'sql' | 'st' | 'swift' | 'tcl' | 'twig' | 'typescript' | 'vb' | 'xml' | 'yaml';
