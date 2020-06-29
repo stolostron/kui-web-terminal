009c314e8ab87b6f4664e2eee67e61800cce9a22
Chris Ahl <cahl@redhat.com>
2020-06-29 16:03:47 -0400

    revert to old message since patternfly required to use new display message

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -31,12 +31,13 @@ import {
 import CustomSearchInput from "@kui-shell/plugin-search/mdist/components/CustomSearchInput"
 import { ClusterUtilization } from "@kui-shell/plugin-kubectl/view-utilization";
 import { ProxyOfflineIndicator } from "@kui-shell/plugin-proxy-support";
-import { productName } from '@kui-shell/client/config.d/name.json'
+import { productName, connectSuccess } from '@kui-shell/client/config.d/name.json'
 const wrapper = document.querySelector(".main");
 if (wrapper) {
   render(
     <Kui bottomInput={<CustomSearchInput/>}
          productName={productName}
+         loadingDone={() => <pre>{connectSuccess}</pre>}
          noPromptContext
          prompt="&#x276f;"
          disableTableTitle
