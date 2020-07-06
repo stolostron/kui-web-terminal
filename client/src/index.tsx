4a4624d0bb6135d84fdf73591dbfc3567bbdb6f3
Chris Ahl <cahl@redhat.com>
2020-07-06 14:27:02 -0400

    change loading message

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -32,10 +32,23 @@ import {
 import CustomSearchInput from "@kui-shell/plugin-search/mdist/components/CustomSearchInput"
 import { ClusterUtilization } from "@kui-shell/plugin-kubectl/view-utilization";
 import { ProxyOfflineIndicator } from "@kui-shell/plugin-proxy-support";
-import { productName, connectSuccess } from '@kui-shell/client/config.d/name.json';
+import { productName, connectSuccess, connecting } from '@kui-shell/client/config.d/name.json';
 
 import { Card } from '@kui-shell/plugin-client-common'
 import { REPL } from '@kui-shell/core'
+
+function loading(repl: REPL) {
+  return (
+    <Card
+      titleInHeader
+      bodyInHeader
+      title={connecting}
+      icon={require('../client-default/icons/png/WelcomeLight.png').default}
+      repl={repl}
+    >
+    </Card>
+  )
+}
 
 function loadingDone(repl: REPL) {
   return (
@@ -51,6 +64,7 @@ function loadingDone(repl: REPL) {
   )
 }
 
+
 // The "Tech Preview" tag is added to the status bar below using the TagWidget
 // While it is not truly a MeterWidget, it is included in that tag as MeterWidgets
 // are positioned by KUI on the right side of the status bar which is the desired position.
@@ -61,6 +75,7 @@ if (wrapper) {
   render(
     <Kui bottomInput={<CustomSearchInput/>}
          productName={productName}
+         loading={loadingDone}
          loadingDone={loadingDone}
          noPromptContext
          prompt="&#x276f;"
