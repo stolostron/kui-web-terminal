d891fbf949379a1b5268ecdfce17d7059ce962ad
Mark Anderson <markande@redhat.com>
2020-07-10 10:41:01 -0400

    add IBM KUI 8.10.12 into release-2.0

---
+++ vendor/client/node_modules/speed-date/lib/speed-date.js
@@ -0,0 +1,47 @@
+var makeFormatter = require('./makeFormatter');
+
+function defaultFormat(utc) {
+  return utc ? 'YYYY-MM-DDTHH:mm:ss[Z]' : 'YYYY-MM-DDTHH:mm:ssZ';
+}
+
+function makeSpeedDate(utc) {
+  function speedDate(fmt, date){
+
+    if(!fmt) fmt = defaultFormat(utc);
+
+    var formatter = makeFormatter(fmt, null, utc);
+
+    if(typeof date === 'undefined'){
+      return formatter;
+    }else{
+      return formatter(date);
+    }
+  }
+
+
+
+  var formatterCache = {};
+
+  speedDate.cached = function(fmt, date){
+
+    if(!fmt) fmt = defaultFormat(utc);
+
+    var formatter = formatterCache[fmt];
+
+    if(formatter === undefined){
+      formatter = formatterCache[fmt] = makeFormatter(fmt, null, utc);
+    }
+
+    if(typeof date === 'undefined'){
+      return formatter;
+    }else{
+      return formatter(date);
+    }
+  };
+
+  return speedDate;
+}
+
+
+module.exports = makeSpeedDate(false);
+module.exports.UTC = makeSpeedDate(true);
