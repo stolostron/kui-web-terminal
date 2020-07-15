5fef6d0ae7237c02d3114e4545b077b464ea86a0
Chris Ahl <cahl@redhat.com>
2020-07-15 19:26:44 -0400

    test

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -5,10 +5,10 @@ export DOCKER_IMAGE_AND_TAG=${1}
 export TEST_IMAGE_AND_TAG=$(echo $DOCKER_IMAGE_AND_TAG | sed 's/kui-web-terminal:/kui-web-terminal-tests:/g')
 
 echo "Running image: ${DOCKER_IMAGE_AND_TAG}"
-sudo make -v -C tests install-oc
-make -v -C tests login-oc
+sudo make -C tests install-oc
+make -C tests login-oc
 # Original way to run tests
-make -v run-all-tests
+make run-all-tests
 
 
 #Possible new way to run tests using dockerized container
