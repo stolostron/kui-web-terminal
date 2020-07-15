a69b380ac8a12a0c79d97372997318e1cda8e8b4
Chris Ahl <cahl@redhat.com>
2020-07-15 11:06:18 -0400

    run e2e tests the original way

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -7,28 +7,29 @@ export TEST_IMAGE_AND_TAG=$(echo $DOCKER_IMAGE_AND_TAG | sed 's/kui-web-terminal
 echo "Running image: ${DOCKER_IMAGE_AND_TAG}"
 sudo make -C tests install-oc
 make -C tests login-oc
-#make run-all-tests
-#make run
+# Original way to run tests
+make run-all-tests
 
 
 
-# get base domain
-if [ -z ${K8S_BASE_DOMAIN} ] ; then
-    K8S_BASE_DOMAIN=`echo ${K8S_CLUSTER_MASTER_IP} | sed 's/^.*\.apps\.//g' | sed 's/\/.*$//g'`
-fi
-
-# set up options.yaml & test-output folder
-mkdir -p test-output
-
-cat << EOF > options.yaml
-options:
-  hub:
-    baseDomain: ${K8S_BASE_DOMAIN}
-    user: ${K8S_CLUSTER_USER}
-    password: ${K8S_CLUSTER_PASSWORD}
-EOF
-
-# start test container
-echo "Testing with image: ${TEST_IMAGE_AND_TAG}"
-#docker run -it --network="host" -e TEST_LOCAL=true --volume $(pwd)/test-output:/results --volume $(pwd)/options.yaml:/resources/options.yaml ${TEST_IMAGE_AND_TAG}
-make run-test-image
+#Possible new way to run tests using dockerized container
+# WORK IN PROGRESS
+#
+## get base domain
+#    K8S_BASE_DOMAIN=`echo ${K8S_CLUSTER_MASTER_IP} | sed 's/^.*\.apps\.//g' | sed 's/\/.*$//g'`
+#fi
+#
+## set up options.yaml & test-output folder
+#mkdir -p test-output
+#
+#cat << EOF > options.yaml
+#options:
+#  hub:
+#    baseDomain: ${K8S_BASE_DOMAIN}
+#    user: ${K8S_CLUSTER_USER}
+#    password: ${K8S_CLUSTER_PASSWORD}
+#EOF
+#
+## start test container
+#echo "Testing with image: ${TEST_IMAGE_AND_TAG}"
+#make run-test-image
