ef9fb1b5065bb6e1c1e6e166556fdaf6c4ae1b70
Mark Anderson <markande@redhat.com>
2020-11-18 17:32:51 -0500

    fix path of /metrics route

--- proxy/app/routes/metrics.js
+++ proxy/app/routes/metrics.js
@@ -7,11 +7,6 @@ const router = express.Router()
 const metricTools = require('../lib/metricsUtils')
 
 router.get('/', (req, res) => {
-    console.log('/')
-    res.sendStatus(200)
-  })
-  
-router.get('/metrics', (req, res) => {
     console.log('/metrics' + ' call from Prometheus');
     res.set('Content-Type', metricTools.promClient.register.contentType);
     res.send(metricTools.promClient.register.metrics());
